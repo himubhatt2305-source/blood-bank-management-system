c0c963f2814ea38df294ae5964b5d323f66756c9
Himanshu <himu.bhatt.2305@gmail.com>
2026-05-07 17:08:45 +0530

    fix server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGO_URI)
 });
 
 /* ---------------- SESSION ---------------- */
-app.set("trust proxy", 1);
+const MongoStore = require("connect-mongo");
 
 app.use(session({
   secret: "bloodbank_secret",
